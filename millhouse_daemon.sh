0bdbd153504f9834fe4a1c1d1988bfe4303242bd
Andrew Johnson <ajohnson@draster.com>
2014-03-21 14:36:50 -0700

    Exit once millhouse is running -- prevents duplicate instances

--- millhouse_daemon.sh
+++ millhouse_daemon.sh
@@ -12,6 +12,7 @@ echo $$ >>/var/run/sasidled.pid
         if [ $? -eq 0 ]; then
                 echo "Starting millhouse..." | logger -i -t sasidle
                 zpool iostat 60 | sh /data/scripts/millhouse.sh -v -t 10 -d da0,da1,da2,da3,da4,da5,da6,da7 | logger -i -t sasidle
+                exit
         fi
         echo "Waiting for a pool to become active..." | logger -i -t sasidle
 done ) &
